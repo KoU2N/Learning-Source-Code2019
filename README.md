cce32497f7e6475f24b0993b766d229d381081e6
kouunn <gao_yun@qq.com>
2019-01-02 20:27:45 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning source code 2019
+
+## 2019学习源码
+
+- [Softmax的Python实现](fluid)
---
+++ softmax/softmax.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Learning Source Code2019
+#
+# Created by: KoU2N
+#
+# 2019年1月1日
+import numpy as np
+def softmax(x):
+    """
+    对输入x的每一行计算softmax。
+
+    该函数对于输入是向量（将向量视为单独的行）或者矩阵（M x N）均适用。
+
+    代码利用softmax函数的性质: softmax(x) = softmax(x + c)
+
+    参数:
+    x -- 一个N维向量，或者M x N维numpy矩阵.
+
+    返回值:
+    x -- 在函数内部处理后的x
+    """
+    # 根据输入类型是矩阵还是向量分别计算softmax
+    if len(x.shape) > 1:
+        # 矩阵
+        tmp = np.max(x, axis=1)  # 得到每行的最大值，用于缩放每行的元素，避免溢出
+        x -= tmp.reshape((x.shape[0], 1))  # 利用性质缩放元素
+        x = np.exp(x)  # 计算所有值的指数
+        tmp = np.sum(x, axis=1)  # 每行求和
+        x /= tmp.reshape((x.shape[0], 1))  # 求softmax
+    else:
+        # 向量
+        tmp = np.max(x)  # 得到最大值
+        x -= tmp  # 利用最大值缩放数据
+        x = np.exp(x)  # 对所有元素求指数
+        tmp = np.sum(x)  # 求元素和
+        x /= tmp  # 求somftmax
+    return x
+
+def main():
+    test1 = np.array([1,  2, 3, 4])
+  
+    print("原始向量:", test1)
+    print("经过softmax后:", softmax(test1))
+
+    test2 = np.array([[1,2],[3,4]])
+    print('原始矩阵:\n', test2)
+
+if __name__ == '__main__':
+    main()
+    print(__name__)
